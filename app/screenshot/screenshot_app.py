e40fcf16cccd29f8f6e60ff3975cc1bdc3d4d33e
Winter021 <rahulkurkurecs@gmail.com>
2024-03-13 02:18:30 +0530

    changes reflected now

--- app/screenshot/screenshot_app.py
+++ app/screenshot/screenshot_app.py
@@ -1,11 +1,14 @@
 import os
 import pyautogui
+import subprocess
+import time
 
 class ScreenshotApp:
     def __init__(self):
         self.folder_path = self.create_images_folder()
         self.screenshot_name = "screenshot.png"
         self.last_screenshot_path = None
+        self.last_active_window = None
 
     def create_images_folder(self):
         folder_name = "images"
@@ -13,11 +16,31 @@ class ScreenshotApp:
             os.makedirs(folder_name)
         return folder_name
 
+    def get_active_window(self):
+        try:
+            result = subprocess.check_output(["xdotool", "getactivewindow", "getwindowname"]).decode("utf-8")
+            return result.strip()
+        except subprocess.CalledProcessError:
+            return None
+
     def capture_screenshot(self):
-        screenshot_path = os.path.join(self.folder_path, self.screenshot_name)
-        pyautogui.screenshot(screenshot_path)
-        print(f"Screenshot captured: {screenshot_path}")
-        self.last_screenshot_path = screenshot_path
+        image_captured = False
+        while not image_captured:
+            # Get the current active window
+            current_active_window = self.get_active_window()
+
+            # Compare with the previous active window
+            if current_active_window != self.last_active_window:
+                # Save the current screenshot
+                screenshot_path = os.path.join(self.folder_path, self.screenshot_name)
+                pyautogui.screenshot(screenshot_path)
+                print(f"Screenshot captured: {screenshot_path}")
+                self.last_screenshot_path = screenshot_path
+                self.last_active_window = current_active_window
+                image_captured = True
+            else:
+                print("No change in active window detected. Waiting for a change...")
+                time.sleep(1)
 
     def delete_last_screenshot(self):
         if self.last_screenshot_path and os.path.exists(self.last_screenshot_path):
@@ -26,3 +49,4 @@ class ScreenshotApp:
             self.last_screenshot_path = None
         else:
             print("No screenshot to delete.")
+
